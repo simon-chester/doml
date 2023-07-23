93d807d13a24c0304ab029e36c1091a3bd6947a8
Simon Chester <schester@vertical-leap.uk>
2023-07-23 21:15:18 +0100

    Beginnings of life days calculator

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,6 +14,17 @@ const routes = [
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
       },
+      {
+        path: '/:year(\\d+)/:month(\\d+)/:day(\\d+)',
+        name: 'LifeDaysCalculator',
+        component: () => import('@/views/LifeDaysCalculator.vue'),
+        props: (route: { params: { day: number; month: number; year: number } }) => ({
+          ...route.params,
+          day: +route.params.day,
+          month: +route.params.month,
+          year: +route.params.year
+      })
+      }
     ],
   },
 ]

2e9ae752fd05740f15c0e4380f43d9dd9b3c72b4
Simon Chester <schester@vertical-leap.uk>
2023-11-11 22:19:12 +0000

    Age on a "card"

--- src/views/LifeDaysCalculator.vue
+++ src/views/LifeDaysCalculator.vue
@@ -1,7 +1,17 @@
 <template>
-    {{ dob.toFormat('dd/MM/yyyy') }}
+    <h1>{{ dobString }}</h1>
 
-    Age: {{ age.toHuman() }}
+    <div style="margin: 1rem;">
+        <v-sheet elevation="4" rounded width="400">
+            <section>
+                <h2>Age</h2>
+
+                <p>{{ age.years.toHuman() }}</p>
+                <p>{{ age.weeks.toHuman() }}</p>
+                <p>{{ age.days.toHuman() }}</p>
+            </section>
+        </v-sheet>
+    </div>
 </template>
 
 <script lang="ts" setup>
@@ -20,8 +30,16 @@
         return DateTime.fromObject({ year, month, day });
     });
 
+    const dobString = computed(() => {
+        return `DOB: ${dob.value.toFormat('dd/MM/yyyy')}`;
+    })
+
     const age = computed(() => {
-        return dob.value.diffNow(['years', 'months', 'days']).negate();
+        return {
+            days:  dob.value.diffNow('days').negate(),
+            weeks:  dob.value.diffNow('weeks').negate(),
+            years:  dob.value.diffNow('years').negate()
+        };
     });
     
 </script>
